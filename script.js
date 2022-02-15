086e20d4bce11c1590641a152116fbe1c184bb14
Gabriel Borges <gabrielborges565@gmail.com>
2022-02-14 23:24:42 -0300

    Contact Page Finished

---
+++ script.js
@@ -0,0 +1,22 @@
+const showAlert = () => {
+    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    var alertTrigger = document.getElementById('liveAlertBtn')
+
+    function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+        alertPlaceholder.append(wrapper)
+    }
+
+    if (alertTrigger) {
+        alertTrigger.addEventListener('click', function () {
+            alert("Congratulations, your form has not been submitted. One day we'll fix this.", 'success')
+        })
+    }
+
+}
+
+
+
+showAlert();
