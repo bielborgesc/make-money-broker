26e197cc12e9a91151ee8651786f0cb9bf1fd2a1
Gabriel Borges <gabrielborges565@gmail.com>
2022-02-15 14:30:08 -0300

    css correction

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const showAlert = () => {
 
     function alert(message, type) {
         let wrapper = document.createElement('div');
-        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+        wrapper.innerHTML = '<div class="alert my-alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
 
         alertPlaceholder.append(wrapper);
     }
